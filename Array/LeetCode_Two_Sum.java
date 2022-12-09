51d55bd21f850a7b6949743f1a5654201a65242b
Mohga Yasser <44075267+mohgayasser@users.noreply.github.com>
2022-12-09 14:52:06 +0200

    Update LeetCode_Two_Sum.java

--- Array/LeetCode_Two_Sum.java
+++ Array/LeetCode_Two_Sum.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        List<Integer> index=new ArrayList<Integer>();
+       
         for(int i=0;i<nums.length;i++){
-               int found=0,newTarget=target-nums[i]; 
-               index.add(i);
+               int newTarget=target-nums[i]; 
+             
                for(int j=i+1;j<nums.length;j++){
                    if(nums[j]==newTarget){
-                        index.add(j);
-                        found=1;
-                        break;
-                   }
+                        return new int[]{i,j};                   }
                }
-               if (found==1) break;
-               index.clear();
+            
         }
-           int [] list =new int[index.size()];
-             for(int i=0;i<index.size();i++){
-                 list[i]=index.get(i);
-             }       
-                    return list;
+     
+       return new int[2];
     }
 }
